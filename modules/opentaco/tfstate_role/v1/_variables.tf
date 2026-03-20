47d3792982715d9fd454f0d4baf15a121076989f
ciloholic <ciloholic0119@gmail.com>
2026-03-20 21:50:36 +0900

    wip

---
+++ modules/opentaco/tfstate_role/v1/_variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "gha_role" {
+  type = string
+}
+
+variable "tfstate_bucket_name" {
+  type = string
+}
